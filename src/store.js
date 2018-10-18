9868ab4417d849a38cf6d3a45df334bebffe4cc6
gillygilstrap <matt.gilstrap0324@gmail.com>
2018-10-17 22:03:03 -0700

    Project Completed

---
+++ src/store.js
@@ -0,0 +1,11 @@
+import { createStore } from 'redux';
+import reducer from './ducks/reducer'
+
+
+export default createStore(reducer);
+
+// import { createStore } from "redux";
+
+// import reducer from "./ducks/reducer";
+
+// export default createStore( reducer );
